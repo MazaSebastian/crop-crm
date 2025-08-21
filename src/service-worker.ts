c20e6c1cd461aa5c88523938652446754f82ee58
MazaSebastian <mazasebastian@hotmail.com>
2025-08-21 11:18:21 -0300

    fix: arreglar service-worker para permitir build exitoso y mostrar enlace Compras en TopNav

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 // Stub para satisfacer Workbox InjectManifest
 // @ts-ignore
 self.__WB_MANIFEST;
