8db14a9c4757e88b6aac52d86a46932f2a9fc12b
MazaSebastian <mazasebastian@hotmail.com>
2026-01-01 14:18:05 -0300

    Debug: Add on-screen OneSignal status to Sidebar for iOS verification

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import OneSignal from 'react-onesignal';
 import styled from 'styled-components';
 import { NavLink, useLocation } from 'react-router-dom';
 import {
